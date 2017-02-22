2a352042bca44b66be2e2a1c1c1f8e61d35eb4f5
msaint72 <mahmut.eren72@gmail.com>
2017-02-22 18:31:53 +0300

    food added

--- src/main/java/com/msaint/myfirstmod/items/ItemTomato.java
+++ src/main/java/com/msaint/myfirstmod/items/ItemTomato.java
@@ -3,12 +3,13 @@ package com.msaint.myfirstmod.items;
 import com.msaint.myfirstmod.MyFirstMod;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 
-public class ItemTomato extends Item {
+public class ItemTomato extends ItemFood {
 	protected String name;
 
 	public ItemTomato(String unlocalizedName) {
-		super();
+		super(4,0.5f,false);
 
 		name = unlocalizedName;
 		this.setUnlocalizedName(unlocalizedName);
